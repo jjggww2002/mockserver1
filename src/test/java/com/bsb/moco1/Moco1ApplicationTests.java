0b5f1dd91f792aa7a4dbf7167fcb31ff125e2ad9
jjggww2002 <jjggww2002@sina.com>
2017-08-01 14:06:26 +0800

    create it

---
+++ src/test/java/com/bsb/moco1/Moco1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bsb.moco1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Moco1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
